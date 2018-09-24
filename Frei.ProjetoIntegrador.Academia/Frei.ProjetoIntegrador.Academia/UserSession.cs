85bb8482fef031d193e22fd4a35a4ffcbe94bddd
Pedro Henrique <PHM1080@outlook.com>
2018-09-24 15:44:25 -0300

    Adicionado Telas, Databases, Business, DTOs; DTOs parcialmente programados.

---
+++ Frei.ProjetoIntegrador.Academia/Frei.ProjetoIntegrador.Academia/UserSession.cs
@@ -0,0 +1,15 @@
+﻿using Frei.ProjetoIntegrador.Academia.DB.Usuario;
+using Nsf._2018.Modulo3.App.DB.Produto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nsf._2018.Modulo3.App
+{
+    static class UserSession
+    {
+        public static UsuarioDTO UsuarioLogado { get; set; }
+    }
+}
