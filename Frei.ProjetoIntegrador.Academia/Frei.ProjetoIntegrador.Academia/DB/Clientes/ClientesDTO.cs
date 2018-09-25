a53a9daaeeb7c790f9de385e9cfc89ef31376bea
lyncoln lima ramos <caixa155@hotmail.com>
2018-09-24 22:51:17 -0300

    dto pronto

--- Frei.ProjetoIntegrador.Academia/Frei.ProjetoIntegrador.Academia/DB/Clientes/ClientesDTO.cs
+++ Frei.ProjetoIntegrador.Academia/Frei.ProjetoIntegrador.Academia/DB/Clientes/ClientesDTO.cs
@@ -8,5 +8,22 @@ namespace Frei.ProjetoIntegrador.Academia.DB.Clientes
 {
     class ClientesDTO
     {
+        public int id_Cliente { get; set; }
+        public int nm_Nome { get; set; }
+        public int ds_CPF { get; set; }
+        public int ds_CEP { get; set; }
+        public int ds_Logradouro { get; set; }
+        public int ds_Bairro { get; set; }
+        public int ds_UF { get; set; }
+        public int ds_Complemento { get; set; }
+        public int ds_Situacao { get; set; }
+        public int num_Telefone { get; set; }
+        public int num_Celular { get; set; }
+        public int ds_Email { get; set; }
+        public int dt_Cadastro { get; set; }
+        public int fk_Cliente_Filial { get; set; }
+        
     }
 }
+
+  
--- Frei.ProjetoIntegrador.Academia/Frei.ProjetoIntegrador.Academia/DB/Estoque/EstoqueDTO.cs
+++ Frei.ProjetoIntegrador.Academia/Frei.ProjetoIntegrador.Academia/DB/Estoque/EstoqueDTO.cs
@@ -8,5 +8,14 @@ namespace Frei.ProjetoIntegrador.Academia.DB.Estoque
 {
     class EstoqueDTO
     {
+        public int id_Estoque { get; set; }
+        public int qnt_Produto { get; set; }
+        public int num_Est_Min { get; set; }
+        public int num_Est_Max { get; set; }
+        
     }
 }
+id_Estoque` INT(11) NOT NULL AUTO_INCREMENT,
+  `qnt_Produto` INT(11) NOT NULL,
+  `num_Est_Min` INT(11) NULL DEFAULT NULL,
+  `num_Est_Max` INT(11) NULL DEFAULT NULL,
