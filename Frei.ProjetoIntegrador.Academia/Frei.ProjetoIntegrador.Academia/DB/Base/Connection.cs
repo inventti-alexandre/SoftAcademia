889424b77e89711c4c14aaf97f09ea1e2a826c70
Pedro Henrique <PHM1080@outlook.com>
2018-09-27 17:00:16 -0300

    Conectado no Servidor de Homologação da ADM

--- Frei.ProjetoIntegrador.Academia/Frei.ProjetoIntegrador.Academia/DB/Base/Connection.cs
+++ Frei.ProjetoIntegrador.Academia/Frei.ProjetoIntegrador.Academia/DB/Base/Connection.cs
@@ -11,7 +11,7 @@ namespace Nsf._2018.Modulo3.App.DB.Base
     {
         public MySqlConnection Create()
         {
-            string connectionString = "server=localhost;database=blackfitdb;uid=root;password=1234;sslmode=none";
+            string connectionString = "server=104.214.59.125;database=blackfitdb;uid=nsf;password=nsf@2018;sslmode=none";
 
             MySqlConnection connection = new MySqlConnection(connectionString);
             connection.Open();
