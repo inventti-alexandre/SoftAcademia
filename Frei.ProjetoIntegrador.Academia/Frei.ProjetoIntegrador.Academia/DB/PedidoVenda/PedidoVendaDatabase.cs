b98d4c8aa9535ec045f838ef876fa6227009a39c
Pedro Henrique <PHM1080@outlook.com>
2018-10-19 15:12:20 -0300

    Módulo de Vendas iniciado

--- Frei.ProjetoIntegrador.Academia/Frei.ProjetoIntegrador.Academia/DB/PedidoVenda/PedidoVendaDatabase.cs
+++ Frei.ProjetoIntegrador.Academia/Frei.ProjetoIntegrador.Academia/DB/PedidoVenda/PedidoVendaDatabase.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,13 @@ namespace Frei.ProjetoIntegrador.Academia.DB.PedidoVenda
 {
     class PedidoVendaDatabase
     {
+        public void RegistrarVenda(PedidoVendaDTO dto)
+        {
+            string script = @"INSERT tb_pedido_venda(dt_Pedido, fk_PedidoVenda_Usuario, fk_PedidoVenda_Cliente, fk_PedidoVenda_Filial)
+                                              VALUES(@dt_Pedido, @fk_PedidoVenda_Usuario, @fk_PedidoVenda_Cliente, @fk_PedidoVenda_Filial)";
+
+            List<MySqlParameter> parms = new List<MySqlParameter>();
+            parms.Add(new MySqlParameter("dt_Pedido", dto.dt_Pedido));
+        }
     }
 }
